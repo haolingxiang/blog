b5525257a451614b9965fc02e8eb653e8e1901e0
haolingxiang <flhao2014@163.com>
2020-04-12 22:00:38 +0800

    first commit

---
+++ src/main/java/com/example/blog/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.example.blog.service;
+
+import com.example.blog.po.Comment;
+
+import java.util.List;
+
+
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+}
